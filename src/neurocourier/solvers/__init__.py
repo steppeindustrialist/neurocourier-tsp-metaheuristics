a7fb87bb943adc99d92bac6954f233053f829ff7
Emre Ünver <190657582+steppeindustrialist@users.noreply.github.com>
2026-01-03 23:47:42 +0300

    Clean packaging and integrate SA/ACO solvers

---
+++ src/neurocourier/solvers/__init__.py
@@ -0,0 +1,11 @@
+from neurocourier.solvers.sa import SAParams, SAResult, simulated_annealing_tsp
+from neurocourier.solvers.aco import ACOParams, ACOResult, ant_colony_optimize
+
+__all__ = [
+    "SAParams",
+    "SAResult",
+    "simulated_annealing_tsp",
+    "ACOParams",
+    "ACOResult",
+    "ant_colony_optimize",
+]
